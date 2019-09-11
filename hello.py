29552d3673de22b2661b338dda425786f83308e0
Dylan Seidletz <djsbug84@gmail.com>
2019-09-10 20:55:24 -0400

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+
+print ("===============")
+print ("  Hello, World")
+print ("    by Dylan")
+print ("===============")
+print("")
+print("Hello, World!")
+name = input("What is your name? ")
+print("Hello, "+ name)
